514955c2e784b606bead86492066aa39f0cf22a0
Jitesh Gamit <59947307+Jitu-U@users.noreply.github.com>
2021-12-11 09:06:26 +0530

    firebaseConfig

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,8 @@
 import { initializeApp } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
+import { firebaseConfig } from './firebaseConfig';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyB1HRDeDEAF-OunMe7kSofkquQ4zBKWP1g",
-    authDomain: "bakemanager.firebaseapp.com",
-    projectId: "bakemanager",
-    storageBucket: "bakemanager.appspot.com",
-    messagingSenderId: "1012570722266",
-    appId: "1:1012570722266:web:a431d2a17a1951ae9d3ae3",
-    measurementId: "G-2E1WB3VQ15"
-  };
+
 
 const firebase = initializeApp(firebaseConfig);
 const storage = getStorage(firebase);
